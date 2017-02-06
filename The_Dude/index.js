299557074a5b0ade27d6949b2de728f91bef2f5f
SeabassW <sebastiaan.westdijk@eneco.com>
2017-02-06 16:19:46 +0100

    Update index.js

--- The_Dude/index.js
+++ The_Dude/index.js
@@ -1,16 +1,17 @@
-{
-  "disabled": false,
-  "bindings": [
-    {
-      "authLevel": "anonymous",
-      "type": "httpTrigger",
-      "direction": "in",
-      "name": "req"
-    },
-    {
-      "type": "http",
-      "direction": "out",
-      "name": "$return"
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello again, " + (req.query.name || req.body.name)
+        };
     }
-  ]
-}
+    else {
+        res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done(null, res);
+};
